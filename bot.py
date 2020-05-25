eba83768b642abb5b39d2c811e784b1dcbfedf7f
Matěj Z <42175875+MatejAZahradnik@users.noreply.github.com>
2020-05-25 16:37:17 +0200

    BOT
    added autorole feature and logging feature

---
+++ bot.py
@@ -0,0 +1,36 @@
+from colored import fore, style
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="guci!")
+TOKEN = "NzE0NDEwODY4OTYzODAzMTY3.Xsunmg.ijGQJr091q6KH_i04OemjWgZXRM"
+
+class log:
+    def status(msg):
+        print(fore.BLUE, '[i]', style.RESET, end="")
+        print(msg)
+
+    def finished(msg):
+        print(fore.GREEN, f'[+]', style.RESET, end="")
+        print(msg)
+
+    def warning(msg):
+        print(fore.ORANGE_1, f'[*]', style.RESET, end="")
+        print(msg)
+
+    def error(msg):
+        print(fore.RED, f'[!]', style.RESET, end="")
+        print(msg)
+
+@bot.event
+async def on_ready():
+    log.status("Loading Token ...")
+    log.finished("Bot has connected to server!")
+
+@bot.event
+async def on_member_join(member):
+    log.status(f"{member.nick} has joined server")
+    role = discord.utils.get(member.server.roles, name="Normální žid")
+    await bot.add_role(member, role)
+    log.finished(f"Member {member.nicj} has been assigned role {role} with name Normální Žid")
+
+bot.run(TOKEN)
